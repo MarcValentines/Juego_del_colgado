45209bc6a17fb059320d2fadad6a1a5ca179cda3
MarcValentines <marcvalentinesmiro@gmail.com>
2024-11-14 15:55:46 +0100

    creación de la palabra secreta
    se va a crear la palabra secreta elegida por el administrador

--- src/Juego_del_colgado/Script_juego.java
+++ src/Juego_del_colgado/Script_juego.java
@@ -1,10 +1,17 @@
 package Juego_del_colgado;
-
+import java.util.Scanner;
 public class Script_juego {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner entradaTec = new Scanner(System.in);
+		
+		String palabraSecreta = "";
+		
+		System.out.println("inserta la palabra secreta = ");
+		palabraSecreta += entradaTec.nextLine().toLowerCase();
+		
+		
 	}
 
 }
